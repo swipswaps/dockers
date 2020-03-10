02013f0026b969c3b480db0cb00700cdea138375
Joe Graham <joe.graham@ag.ny.gov>
2020-03-10 11:51:34 -0400

    fix notebook config

--- dumpling/jupyter_notebook_config.py
+++ dumpling/jupyter_notebook_config.py
@@ -1,5 +1,9 @@
 import socket
 
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# TODO: change IP
+s.connect(('8.8.8.8', 1))
+local_ip = s.getsockname()[0]
 
-c.NotebookApp.ip = socket.getfqdn()
+c.NotebookApp.ip = local_ip
 c.NotebookApp.open_browser = False
